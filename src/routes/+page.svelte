c7389ccbfd59990ed4963a50679bb7e1a27a8c9d
choi2j <ritaliz1032@gmail.com>
2023-02-12 10:21:12 +0900

    home, roomList page done without functions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,56 @@
 <script>
 	import Icon from 'svelte-awesome';
-	import { search, download, upload, gears, lock, github, instagram, envelopeSquare, users } from 'svelte-awesome/icons';
+	import {
+		search,
+		download,
+		upload,
+		gears,
+		lock,
+		github,
+		instagram,
+		envelopeSquare,
+		users
+	} from 'svelte-awesome/icons';
+
+	let user = true;
+	let src = 'src/img/banner.svg';
 </script>
 
 <svelte:head>
+	<link rel="stylesheet" href="src/css/home.css" />
 </svelte:head>
 
 <main>
-    
+	<div class="top">
+		<div class="top-text">
+			<p class="p-bigxl">
+				<span class="orng-color p-bigxb">Submit</span> your
+				<span class="blue-color p-bigxb">Work</span><br />
+				오직 당신의 <span class="orng-color p-bigxb">과제</span>를 위한
+				<span class="blue-color p-bigxb">서비스</span>
+			</p>
+		</div>
+		<div class="top-img">
+			<img {src} alt="banner-img" />
+		</div>
+	</div>
+	<div class="bot">
+		<div class="bot-buttons">
+			{#if user == false}
+                <a href="/login" class="a-big blue-back mg-sml">방 생성하기</a>
+                <a href="/register" class="a-big orng-back mg-sml">방 참가하기</a>
+			{:else}
+			    <a href="/roomMake" class="a-big blue-back mg-sml">방 생성하기</a>
+			    <a href="/roomList" class="a-big orng-back mg-sml">방 참가하기</a>
+			{/if}
+		</div>
+		<div class="bot-contact">
+			<p class="p-bigl">Contact</p>
+			<div class="bot-contact-buttons">
+				<a href="/" class="mg-sml"><Icon data={github} scale={3} /></a>
+				<a href="/" class="mg-sml"><Icon data={instagram} scale={3} /></a>
+				<a href="/" class="mg-sml"><Icon data={envelopeSquare} scale={3} /></a>
+			</div>
+		</div>
+	</div>
 </main>
-
-<style>
-</style>
---
+++ src/routes/roomList/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	let rooms = [
+		{
+			roomName: 'QWERTY',
+			roomID: '000000',
+			roomDes: '선린 웹 동아리 쿼티의 공식 MitSub 방입니다.'
+		},
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8
+	];
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="src/css/roomList.css" />
+</svelte:head>
+
+<main>
+	<div class="room-search mg-cen">
+		<p class="p-bigxb mg-sml">방 찾기</p>
+		<p class="p-smll mg-sml">숙제를 제출할 방을 찾아주세요</p>
+		<input type="text" class="inp mg-sml" placeholder="방 이름 또는 아이디 검색" />
+	</div>
+	<ul class="room-list mg-cenX">
+		{#each rooms as room, i}
+			<li class="mg-big">
+				<div class="room pd-sml">
+					<div class="room-banner" />
+					<div class="room-text pd-big">
+						<a href="/roomList">
+							<span class="p-bigb">{room.roomName}</span> <span class="p-smll">{room.roomID}</span>
+						</a>
+						<p class="p-smll">{room.roomDes}</p>
+					</div>
+				</div>
+			</li>
+		{/each}
+	</ul>
+</main>
